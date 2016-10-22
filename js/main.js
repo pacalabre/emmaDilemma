689d66ae373b0f56d0d22e62995c77b2ff9dbfc6
Paul <pacalabre@gmail.com>
2016-10-22 16:08:35 -0700

    added modals into each project block on the home page - working on building the modal.

--- js/main.js
+++ js/main.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
   $(".button-collapse").sideNav();
 })
 
+// Bitters Modal
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
 
