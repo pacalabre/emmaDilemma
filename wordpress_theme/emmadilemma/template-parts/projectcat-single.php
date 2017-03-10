00c871610977d009de4067adeb164b346249df5b
Paul <pacalabre@gmail.com>
2017-03-10 10:30:41 -0800

    changed the path on the work nav to direct links rather than page numbers

--- wordpress_theme/emmadilemma/template-parts/projectcat-single.php
+++ wordpress_theme/emmadilemma/template-parts/projectcat-single.php
@@ -1,8 +1,8 @@
 <section class="row">
     <div class="col s12 category-nav">
         <a class="category-nav-item" href="/work"><h3 class="category-nav-item-h3">All</h3></a>
-        <a class="category-nav-item" href="/index.php?p=189"><h3 class="category-nav-item-h3">Web</h3></a>
-        <a class="category-nav-item" href="/index.php?p=191"><h3 class="category-nav-item-h3">Advertising</h3></a>
-        <a class="category-nav-item" href="/index.php?p=193"><h3 class="category-nav-item-h3">Funsies</h3></a>
+        <a class="category-nav-item" href="/work/web"><h3 class="category-nav-item-h3">Web</h3></a>
+        <a class="category-nav-item" href="/work/advertising"><h3 class="category-nav-item-h3">Advertising</h3></a>
+        <a class="category-nav-item" href="/work/funsies"><h3 class="category-nav-item-h3">Funsies</h3></a>
     </div>
 </section>
